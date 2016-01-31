29b9da39f5a597e145196e0b66350d5249085a04
Lubomir Geshev <lubo_geshev@abv.bg>
2016-01-31 21:19:04 +0200

    TopNews singleton simplified

--- src/News/TopNews.java
+++ src/News/TopNews.java
@@ -1,19 +1,33 @@
 package News;
 
-public class TopNews extends News{
-	
-	public TopNews(String text) {
+public class TopNews extends News {
+
+	private static TopNews uniqueInstance;
+
+	private TopNews() {
+
+	}
+
+	private TopNews(String text) {
 		super(text);
 		// this.getTitle() BIGGER FONT
 		// this.getText() BIGGER FONT
 	}
-	
-	public TopNews(String title, String text) {
+
+	private TopNews(String title, String text) {
 		super(title, text);
 		// this.getTitle() BIGGER FONT
 		// this.getText() BIGGER FONT
 	}
-	
-	
+
+	public static TopNews getTopNews() {
+		if (uniqueInstance == null)
+			uniqueInstance = new TopNews();
+		return uniqueInstance;
+	}
+
+	public static void setTopNews(News latest) {
+		TopNews.uniqueInstance = (TopNews) latest;
+	}
 
 }
