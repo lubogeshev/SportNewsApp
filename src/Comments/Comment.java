62c3e43c8b8c719f6a73882925e11635234082be
Lubomir Geshev <lubo_geshev@abv.bg>
2016-02-04 08:46:41 +0200

    Date field, getters added

--- src/Comments/Comment.java
+++ src/Comments/Comment.java
@@ -1,8 +1,28 @@
 package Comments;
 
+import java.time.LocalDateTime;
+
 public class Comment {
 
-	String author;
-	String text;
+	private String author;
+	private String text;
+	private final LocalDateTime commentedOn = LocalDateTime.now().withNano(0);
+	
+	public Comment(String author, String text) {
+		this.author = author;
+		this.text = text;
+	}
+
+	public LocalDateTime getCommentedOn() {
+		return commentedOn;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public String getText() {
+		return text;
+	}	
 	
 }
