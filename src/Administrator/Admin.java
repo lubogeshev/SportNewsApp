90f2b8f3777947d7286aef834804b3c6241abed1
Lubomir Geshev <lubo_geshev@abv.bg>
2016-02-08 00:30:55 +0200

    Registration now leads to admin adding in list of admins

--- src/Administrator/Admin.java
+++ src/Administrator/Admin.java
@@ -10,12 +10,13 @@ public class Admin {
 		this.password = password;
 	}
 	
-	public static Admin registerAsAdmin (String name, String password) {
-		return new Admin(name, password);
+	public static void registerAsAdmin (String name, String password) {
+		NewsManager.getAdmins().add(new Admin(name, password));
 	}
 	
+	
 	public void loginAsAdmin (String name, String password) {
-		if (name.equals(this.adminName) & password.equals(this.password)) {
+		if (name.equals(this.adminName) && password.equals(this.password)) {
 			System.out.println("Login successful!");
 		} else {
 			System.out.println("Login unsuccessful!");
