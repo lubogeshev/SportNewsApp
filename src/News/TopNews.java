f2f8bf245cf5b789162e609216e227057bd18594
Lubomir Geshev <lubo_geshev@abv.bg>
2016-02-08 00:33:41 +0200

    Constructors modified

--- src/News/TopNews.java
+++ src/News/TopNews.java
@@ -4,30 +4,27 @@ public class TopNews extends News {
 
 	private static TopNews uniqueInstance;
 
-	private TopNews() {
-
-	}
-
-	private TopNews(String text) {
-		super(text);
+	private TopNews(String text, Category category) {
+		super(text, category);
 		// this.getTitle() BIGGER FONT
-		// this.getText() BIGGER FONT
+		// this.getImage() BIGGER PIC
 	}
 
-	private TopNews(String title, String text) {
-		super(title, text);
+	private TopNews(String title, String text, Category category) {
+		super(title, text, category);
 		// this.getTitle() BIGGER FONT
-		// this.getText() BIGGER FONT
+		// this.getImage() BIGGER PIC
 	}
 
-	public static TopNews getTopNews() {
-		if (uniqueInstance == null)
-			uniqueInstance = new TopNews();
+	public static TopNews createTopNews(String title, String text, Category category) {
+		if (uniqueInstance == null) {
+			if (title == null) {
+				uniqueInstance = new TopNews(text, category);
+			} else {
+				uniqueInstance = new TopNews(title, text, category);
+			}
+		}
 		return uniqueInstance;
-	}
-
-	public static void setTopNews(News latest) {
-		TopNews.uniqueInstance = (TopNews) latest;
 	}
 
 }
