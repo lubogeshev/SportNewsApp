b9356684a7f503efe7a11dd3460d6aa46295c351
Lubomir Geshev <lubo_geshev@abv.bg>
2016-02-08 00:31:08 +0200

    Demo Cleared

--- src/Demo/Demo.java
+++ src/Demo/Demo.java
@@ -1,17 +1,8 @@
 package Demo;
 
-import Comments.Comment;
-import News.TopNews;
-
 public class Demo {
 
 	public static void main(String[] args) {
-	
-		TopNews news = TopNews.getTopNews();
-		news.printNews();
-		Comment comment = new Comment("","");
-		
-		System.out.println(comment.getCommentedOn());
 		
 	}
 	
