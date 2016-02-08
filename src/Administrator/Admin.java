284750a91a4adf2195dfd2351e41ec8bfa2aeed7
Lubomir Geshev <lubo_geshev@abv.bg>
2016-02-08 11:52:38 +0200

    Verify Pass added

--- src/Administrator/Admin.java
+++ src/Administrator/Admin.java
@@ -4,24 +4,32 @@ public class Admin {
 
 	private String adminName;
 	private String password;
-	
+
 	private Admin(String name, String password) {
 		this.adminName = name;
 		this.password = password;
 	}
-	
-	public static void registerAsAdmin (String name, String password) {
-		NewsManager.getAdmins().add(new Admin(name, password));
+
+	public static void registerAsAdmin(String name, String password1, String password2) {
+		if (verifyPassword(password1, password2)) {
+			NewsManager.getAdmins().add(new Admin(name, password1));
+		}
 	}
-	
-	
-	public void loginAsAdmin (String name, String password) {
+
+	public void loginAsAdmin(String name, String password) {
 		if (name.equals(this.adminName) && password.equals(this.password)) {
 			System.out.println("Login successful!");
 		} else {
 			System.out.println("Login unsuccessful!");
 		}
 	}
-	
-	
+
+	private static boolean verifyPassword(String password1, String password2) {
+		if (password1.equals(password2)) {
+			return true;
+
+		}
+		return false;
+	}
+
 }
